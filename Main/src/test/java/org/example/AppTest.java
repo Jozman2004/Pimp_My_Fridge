485078f26f0b0421b40b245947edd0adfce7bd47
Jozman2004 <joseph.nkoulou@2027.ucac-icam.com>
2023-12-05 14:38:24 +0100

    1ere interface

--- Main/src/test/java/org/example/AppTest.java
+++ Main/src/test/java/org/example/AppTest.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple View.
  */
 public class AppTest 
     extends TestCase
